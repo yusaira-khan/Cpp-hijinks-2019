6baaf503b2170ceb0eb6a921d801893b7a0f9999
Yusaira Khan <yusaira.khan@mail.mcgill.ca>
2019-07-07 01:44:03 -0400

    tentative adding edgelist

--- Djistra/src/Graph.cpp
+++ Djistra/src/Graph.cpp
@@ -2,42 +2,70 @@
 #include <iostream>
 #include <vector>
 #include <queue>
-
+#include <pair>
+#include <random>
 using std::list;
 using std::vector;
 namespace Djistra{
+// const bool UNDIRECTED = true;
+// class Edge{
+// }
+// class Vertex{
+// }
+typedef int32_t Vertex;
+typedef std::pair<Vertex,Vertex> Edge;
 
-class Edge{
-}
-class Vertex{
-}
-class Graph {
+class UndirectedGraph {
 public:
-	Graph(int numVertices,int density){
+	UndirectedGraph(int numVertices,float density)
+	:edgelist(0)
+	{
+		this.numVertices = numVertices;
+		this.initialDensity = density;
 
+ 		std::default_random_engine eng((std::random_device())());
+ 		std::uniform_real_distribution<float> edgeProbability(0, 1.0);
+ 		for (int i =0; i < numVertices; i++){
+			for int j = i+1; j < numVertices; j++;{
+				if (edgeProbability() < density){
+				edgelist.prepend(new Edge(i,j));
+			}
+			}
+		}
 	}
-	~Graph(){
+	~UndirectedGraph(){
 		delete edgelist;
 		delete adjacencyList;
 		delete adjacencyMatrix;
 	};
-	getStr();
-	getNumVertices();
-	getNumEdges();
-	getShortestPath();
-	isConnected();
-	getCurrentDensity();
-	getAverageDegree();
+	string getStr(){
+		string res = "";
+		for (e in edgelist){
+			e[0]+"<-->"+e[1];
+		}
+	}
+	int getNumVertices(){
+		return numVertices;
+	};
+	int getNumEdges(){
+		return edgelist.size();
+	}
+	string getShortestPath();
+	bool isConnected();
+	float getCurrentDensity();
+	float getAverageDegree();
 
 private:
-		std::list<Edge> edgelist;
+		list<Edge> edgelist;
 		list<list<Vertex>> adjacencyList;
 		vector<vector<Vertex>> adjacencyMatrix;
 		const int numVertices;
-		const int initialDensity;
+		const float initialDensity;
+		void alignReprWEdgelist();
+
 
 }
-ostream & operator << (ostream &out, const Graph &c)
+ostream & operator << (ostream &out, const UndirectedGraph &c)
 {
     out << c.getStr() << endl;
     return out;
